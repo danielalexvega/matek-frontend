19ba5200175ebe4dfa5a2c91f92cacbce23f61a1
Daniel Vega <danielalexvega@gmail.com>
2021-08-30 21:52:15 -0500

    work on NewProblem and Input components

--- src/problems/pages/NewProblem.js
+++ src/problems/pages/NewProblem.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/Input';
+import "./NewProblem.css";
+
 const NewProblem = () => {
     return (
-        <div>
-            NEW PROBLEM
-        </div>
+        <form className="problem-form">
+            <Input element="input" type="text" label="Title" validators={[]} errorText="Please enter a valid title"/>
+        </form>
     )
 }
 
