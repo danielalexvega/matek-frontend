a03b7d3d014385c1c46b1ccf553115f3ebb3c316
Daniel Vega <danielalexvega@gmail.com>
2021-10-05 23:05:21 -0500

    work on login function

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -69,11 +69,37 @@ const Auth = () => {
 
   const authSubmitHandler = async (event) => {
     event.preventDefault();
-
+    
+    setIsLoading(true);
     if (isLoginMode) {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        setIsLoading(false);
+
+        login();
+        routeChange();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || "Something went wrong. Please try again.");
+      }
+
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch("http://localhost:5000/api/users/signup", {
           method: "POST",
           headers: {
@@ -91,13 +117,11 @@ const Auth = () => {
           throw new Error(responseData.message);
         }
 
-        console.log(responseData);
         setIsLoading(false);
 
         login();
         routeChange();
       } catch (err) {
-        console.log(err);
         setIsLoading(false);
         setError(err.message || "Something went wrong. Please try again.");
       }
