c3dfe1191548f40dee11026cc0d6cf14f5af0b07
Daniel Vega <danielalexvega@gmail.com>
2021-10-10 12:36:01 -0500

    rework AllProblem.js to fetch data and not use DummyData

--- src/problems/pages/AllProblems.js
+++ src/problems/pages/AllProblems.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 import ProblemList from "../components/ProblemList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import "./AllProblems.css";
 
-import DUMMY_PROBLEMS from "../../shared/DUMMY_PROBLEMS";
-
 const AllProblems = () => {
+  const [loadedProblems, setLoadedProblems] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  useEffect(() => {
+    const fetchProblems = async () => {
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/problems/"
+        );
+        setLoadedProblems(responseData.problems);
+      } catch (error) {}
+    };
+
+    fetchProblems();
+  }, [sendRequest]);
+
   return (
     <div className="all-problems__container">
+      <ErrorModal error={error} onClear={clearError} />
       <h1>Here are all the problems</h1>
-      <ProblemList problems={DUMMY_PROBLEMS} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedProblems && (
+        <ProblemList problems={loadedProblems} />
+      )}
     </div>
   );
 };
