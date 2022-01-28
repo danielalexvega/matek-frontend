e4808bc11e90e69dcd139a091947c7bf0176ffbc
Daniel Vega <danielalexvega@gmail.com>
2022-01-27 23:02:58 -0600

    work on Learn More page

--- src/homepage/components/FullWidthContainer.js
+++ src/homepage/components/FullWidthContainer.js
@@ -5,13 +5,14 @@ import "./FullWidthContainer.css";
 const FullWidthContainer = (props) => {
   const backgroundColor = props.backgroundColor ? props.backgroundColor : "";
   const color = props.color ? props.color : "";
+  const className = props.className ? props.className : "";
 
   return (
     <div
-      className="full-width-container"
+      className={`full-width-container ${className}`}
       style={{ backgroundColor: `${backgroundColor}`, color: `${color}` }}
     >
-      <div className="full-width-container__content" style={{width: `${props.width}`}}>
+      <div className={`full-width-container__content ${className}`} style={{width: `${props.width}`}}>
         <h3 className="full-width-container__title">{props.title}</h3>
         <p className="full-width-container__body">{props.children}</p>
       </div>
--- src/learn-more/pages/LearnMore.js
+++ src/learn-more/pages/LearnMore.js
@@ -1,12 +1,57 @@
 import React from "react";
+import FullWidthContainer from "../../homepage/components/FullWidthContainer";
+import { Tex } from "react-tex";
 
 import "./LearnMore.css";
 
 const LearnMore = () => {
     return (
-        <div>
-            <h3 className="learn-more__header">Coming soon...</h3>
+        <div className="page-container">
+
+            <FullWidthContainer
+                backgroundColor="#EDF6FC"
+                color="#061523"
+                title="Why Matek?"
+                width="75%"
+            >
+                We have one goal for Matek; to make teaching highschool math easier. We know that each school is full of the most knowledgable teachers and we want to tap into that. It will start with a problem database, and from there we'll start conversations with teachers and ask what would make the day to day routine easier for them and also for their students.
+            </FullWidthContainer>
+            <FullWidthContainer
+                backgroundColor="#EDF6FC"
+                color="#061523"
+                width="75%"
+                className="my-0"
+            >
+               As teachers sign up, the problem database will grow. We want to have hundres of problems for each content domain and every course. We'll start by focusing on Algebra 1, Algebra 2, and PreCalculus, and eventually move into Geometry and Calculus.
+            </FullWidthContainer>
         </div>
+
+        // <div className="learn-more__page page-container">
+        //     <div className="page-container__body">
+        //         <h2 className="body__header">Why Matek?</h2>
+        //         <div className="body__content">
+        //             <h3 className="content__subheader">Actually making the job easier.</h3>
+        //             <p className="three-quarters">
+        //                 We have one goal for Matek; to make teaching highschool math easier. We know that each school is full of the most knowledgable teachers and we want to tap into that. It will start with a problem database, and from there we'll start conversations with teachers and ask what would make the day to day routine easier for them and also for their students.
+        //             </p>
+        //             <p className="three-quarters">
+        //                 As teachers sign up, the problem database will grow. We want to have hundres of problems for each content domain and every course. We'll start by focusing on Algebra 1, Algebra 2, and PreCalculus, and eventually move into Geometry and Calculus.
+        //             </p>
+        //         </div>
+        //     </div>
+        //     <div className="page-container__body">
+        //         <h2 className="body__header">What is Katex?</h2>
+        //         <div className="body__content">
+        //             <h3 className="content__subheader">Actually making the job easier.</h3>
+        //             <p className="three-quarters">
+        //                 We have one goal for Matek; to make teaching highschool math easier. We know that each school is full of the most knowledgable teachers and we want to tap into that. It will start with a problem database, and from there we'll start conversations with teachers and ask what would make the day to day routine easier for them and also for their students.
+        //             </p>
+        //             <p className="three-quarters">
+        //                 As teachers sign up, the problem database will grow. We want to have hundres of problems for each content domain and every course. We'll start by focusing on Algebra 1, Algebra 2, and PreCalculus, and eventually move into Geometry and Calculus.
+        //             </p>
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
