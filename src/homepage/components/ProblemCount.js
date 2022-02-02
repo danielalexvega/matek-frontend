39a4bfd6a93c97a8fa0c9e8e623765c8ce6d3c7b
Daniel Vega <danielalexvega@gmail.com>
2022-02-01 23:43:03 -0600

    work on style for ProblemCount

--- src/homepage/components/ProblemCount.js
+++ src/homepage/components/ProblemCount.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 import "./ProblemCount.css";
 
 const ProblemCount = () => {
+    const [problemCount, setProblemCount] = useState(0);
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+    useEffect(() => {
+        const fetchProblems = async () => {
+            try {
+                const { problems } = await sendRequest(
+                    process.env.REACT_APP_BACKEND_URL + "/problems/"
+                );
+                setProblemCount(problems.length);
+            } catch (err) {}
+        };
+
+        fetchProblems();
+    }, [sendRequest]);
+
     return (
         <div className="problem-count">
+            <ErrorModal error={error} onClear={clearError} />
             <div className="problem-count__container">
                 <div className="container__left-side">
                     <h3>Growing and growing...</h3>
@@ -13,7 +33,19 @@ const ProblemCount = () => {
                         add to our numbers
                     </p>
                 </div>
-                <div className="container__right-side"></div>
+                <div className="container__right-side">
+                    {isLoading && (
+                        <div className="center">
+                            <LoadingSpinner />
+                        </div>
+                    )}
+                    {!isLoading && problemCount && (
+                        <div className="right-side__problemCount-container">
+                            <div className="problemCount-container__count light">{problemCount}</div>
+                            <div className="problemCount-container__text light">problems</div>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
